7977feaca9b556f621fbf6ba4ab92371aaebd3b3
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 20:37:18 +0000

    stubbed out interface

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,9 +2,22 @@ require 'pry'
 
 class CLI
 
-  def start
+  #start method that welcomes user
 
-  end
+  #ask for user to input zipcode
+
+  #scrape data using zipcode as argument
+  #instantiate a new City object using scraped data
+
+  #provide menu of options
+    #see current weather
+      #would you like to see additional weather details?
+    #see astronomical data
+      #sunrise/sunset
+      #moonrise/moonset
+      #visible planets tonight
+    #exit the program
+      #print goodbye message
 
 
 
