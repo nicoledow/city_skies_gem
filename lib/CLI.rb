9dc7caffefa4ec986acd3399d8bfc18c1882ca19
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 12:31:11 +0000

    begin method for cli

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,8 +2,11 @@ class CLI
 
   def begin
     puts "Hello! Welcome to the CitySkies gem."
+    puts "Please enter a 5-digit U.S. zip code to see weather or celestial information. "
+    zipcode = gets.strip
+    zipcode
   end
-  
+
 
 
 
