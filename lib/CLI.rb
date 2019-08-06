47d9959c8bfbc0f6eff596f59baa7afb338dfb07
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 12:46:31 +0000

    added menu method to cli

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -12,7 +12,15 @@ class CLI
     zipcode
   end
 
-
+  def menu
+    puts "What would you like to know about your city? Please enter a number 1-5."
+    puts "1. Current weather"
+    puts "2. Visible planets for tonight"
+    puts "3. Current moon phase"
+    puts "4. Sunrise and sunset times"
+    puts "5. Moonrise and moonset times"
+    #puts "6. All yearly meteor showers"
+  end
 
 
 
