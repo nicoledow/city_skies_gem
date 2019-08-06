09b9722b1a696ca32c94dbf99b4fd51416668215
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 16:15:57 +0000

    added scraper for current temperature

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -6,6 +6,7 @@ class Scraper
 
   def scrape_weather(zipcode)
     weather_info = Nokogiri::HTML(open("https://www.timeanddate.com/weather/@z-us-#{zipcode}"))
+    current_temp = weather_info.css("#bk-focus .fixed #qlook .h2").text
     binding.pry
   end
 
