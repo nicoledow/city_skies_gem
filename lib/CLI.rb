17a341635d65e4139844f60cfdaeb9136eb971da
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 20:43:31 +0000

    added start and get_zipcode methods

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -3,8 +3,18 @@ require 'pry'
 class CLI
 
   #start method that welcomes user
+  def self.start
+    puts "Hi! Welcome to the CitySkies gem."
+    get_zipcode
+  end
 
   #ask for user to input zipcode
+  def self.get_zipcode
+    puts "Please enter a 5-digit U.S. zip code to see information on that city."
+    zipcode = gets.strip.to_s
+    binding.pry
+    zipcode
+  end
 
   #scrape data using zipcode as argument
   #instantiate a new City object using scraped data
@@ -22,3 +32,4 @@ class CLI
 
 
 end
+CLI.get_zipcode
