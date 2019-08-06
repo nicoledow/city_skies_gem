841e664932b32cf2862ea0180094b136ff401a51
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 20:54:30 +0000

    formatting 'run' method

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,19 +1,33 @@
 require 'pry'
 
 class CLI
+  @@zipcode = ""
 
   #start method that welcomes user
   def self.start
     puts "Hi! Welcome to the CitySkies gem."
-    get_zipcode
+    run
   end
 
   #ask for user to input zipcode
-  def self.get_zipcode
+  # def get_zipcode
+  #   puts "Please enter a 5-digit U.S. zip code to see information on that city."
+  #   zipcode = gets.strip.to_s
+  #   zipcode
+  # end
+
+  def run
     puts "Please enter a 5-digit U.S. zip code to see information on that city."
     zipcode = gets.strip.to_s
-    binding.pry
-    zipcode
+    @@zipcode = zipcode
+
+    city_weather = Scraper.scrape_weather(zipcode)
+    city_astronomical_data = Scraper.scrape_astronomy(zipcode)
+    city = City.new(city_weather, city_astronomical_data)
+
+
+
+
   end
 
   #scrape data using zipcode as argument
