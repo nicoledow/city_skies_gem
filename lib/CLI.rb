bb8227bfac7abc44b2fd3a3490c8ebfbdf1dd96b
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 12:38:29 +0000

    added get_zipcode method

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -4,6 +4,9 @@ class CLI
 
   def begin
     puts "Hello! Welcome to the CitySkies gem."
+  end
+
+  def get_zipcode
     puts "Please enter a 5-digit U.S. zip code to see weather or celestial information. "
     zipcode = gets.strip
     zipcode
