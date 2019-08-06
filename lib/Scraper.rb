bc2e01df2c7b1422866ca356b6cc67110b95d458
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 16:43:22 +0000

    added css selectors for weather_description

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -4,9 +4,12 @@ require 'open-uri'
 
 class Scraper
 
+#currently doesn't scrape zipcodes starting with 0+digit greater than 7
   def scrape_weather(zipcode)
+    zipcode = zipcode.to_s
     weather_info = Nokogiri::HTML(open("https://www.timeanddate.com/weather/@z-us-#{zipcode}"))
     current_temp = weather_info.css("#bk-focus .fixed #qlook .h2").text
+    weather_description = weather_info.css("#bk-focus .fixed #qlook p").first.text
     binding.pry
   end
 
