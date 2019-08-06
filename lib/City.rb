126ffbc46c3bdb96b58f56b7e981d41ce185dd76
Nicole Dow <nikki.dow71@gmail.com>
2019-08-06 18:56:30 +0000

    refactoring planet data

--- lib/City.rb
+++ lib/City.rb
@@ -6,8 +6,6 @@ require 'open-uri'
 class City
   attr_accessor :zipcode, :name, :current_temp, :weather_description, :Mercury, :Venus, :Mars, :Jupiter, :Saturn, :Uranus, :Neptune
 
-  @@all = []
-
   def initialize(weather_hash, astronomy_hash)
     weather_hash.each do |k, v|
       self.send("#{k}=", v)
@@ -16,11 +14,14 @@ class City
     astronomy_hash.each do |k, v|
       self.send("#{k}=", v)
     end
-binding.pry
-    @@all << self
   end
+
 
 
 end
 
-City.new(Scraper.scrape_weather("85259"), Scraper.scrape_astronomy("85259"))
+# City.new(Scraper.scrape_weather("85259"), Scraper.scrape_astronomy("85259"))
+weather = Scraper.scrape_weather("85259")
+astronomy = Scraper.scrape_astronomy("85259")
+scottsdale = City.new(weather, astronomy)
+scottsdale.get_planet_info
