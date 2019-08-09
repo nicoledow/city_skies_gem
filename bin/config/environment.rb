d45d6f5c8b7e61ac0467070bb664f4f71a396376
Nicole Dow <nikki.dow71@gmail.com>
2019-08-09 23:45:46 +0000

    saving before messing with CLI call

--- bin/config/environment.rb
+++ bin/config/environment.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'pry'
 require 'colorize'
 
-require_relative ".../lib/Scraper"
+require_relative ".../lib/Scraper.rb"
 require_relative ".../lib/City"
 require_relative ".../lib/CLI"
 require_relative "../lib/city_skies"
