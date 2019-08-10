c10b28b94395c705f2dc3be9bdd59d48e5ab84fd
Nicole Dow <nikki.dow71@gmail.com>
2019-08-10 00:33:04 +0000

    made program run from the bin folder

--- bin/config/environment.rb
+++ bin/config/environment.rb
@@ -2,8 +2,3 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 require 'colorize'
-
-require_relative ".../lib/Scraper.rb"
-require_relative ".../lib/City"
-require_relative ".../lib/CLI"
-require_relative "../lib/city_skies"
--- lib/city_skies.rb
+++ lib/city_skies.rb
@@ -1,4 +1,4 @@
 require_relative "./city_skies/version"
 require_relative "./CLI"
 
-CLI.new.start
+# CLI.new.start
